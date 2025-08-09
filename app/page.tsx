dbc5dcf4b551e5528a318914836ad64f499a6ac6
salim <salimfardeheb442@gmail.com>
2025-08-10 00:52:24 +0100

    feat front login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
     <>
-      <div className="bg-[#06B9AE] p-8 md:p-14 flex flex-col items-center gap-y-8 rounded-lg shadow-2xl">
+      <div className="bg-[#06B9AE] p-8 md:p-x-14 flex flex-col items-center gap-y-8 rounded-lg shadow-2xl md:w-1/2">
         <p className="text-4xl font-black text-white">Connexion</p>
-        <form className="flex flex-col gap-4">
-          <div>
+        <form className="space-y-6 flex flex-col items-center w-full">
+          <div className="flex flex-col gap-10 w-full">
             <input
               type="text"
               placeholder="Nom de la boutique"
@@ -17,7 +17,7 @@ export default function Home() {
             />
           </div>
           <div>
-            <button>Se connecter</button>
+            <button className="bg-[#F39C12] md:px-14 px-7 py-1 rounded-md text-white font-bold hover:opacity-90 cursor-pointer">Se connecter</button>
           </div>
         </form>
       </div>
