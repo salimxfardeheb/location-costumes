d1d869ca3a91f3b994d558df4adccc0c9d1b22d9
salim <salimfardeheb442@gmail.com>
2025-08-15 15:39:34 +0100

    feat add shoe & accessory models front end

--- app/[nom_boutique]/profile/[category]/addModel/components/accessory.tsx
+++ app/[nom_boutique]/profile/[category]/addModel/components/accessory.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
 
 const accessory = () => {
+  const sizes = ["Cravate", "Papillion", "Ceinture", "Montre"];
   return (
     <div>
-      addaccessory
-    </div>
-  )
-}
+      <form className="flex flex-col justify-center items-start mx-auto w-fit space-y-8">
+        <label
+          htmlFor="taille"
+          className="flex gap-10 justify-between w-2/5 text-nowrap "
+        >
+          <span className="text-xl">selectionner le model :</span>
+          <ul className="flex gap-10">
+            {sizes.map((size) => (
+              <li key={size} className="flex gap-2.5">
+                <input type="radio" />
+                <p className="text-nowrap">{size}</p>
+              </li>
+            ))}
+          </ul>
+        </label>
+        <label
+          htmlFor="model"
+          className="flex justify-start items-center gap-10"
+        >
+          <span className="text-xl text-nowrap">Description :</span>
+          <input
+            type="text"
+            placeholder="Description"
+            className="bg-[#B6FFF6] px-5 py-2 rounded-xl border-2 border-[#36CBC1] placeholder:text-gray-600 focus-within:placeholder:text-[#36CBC1] focus-within:outline-0 min-w-full"
+          />
+        </label>
 
-export default accessory
+        <label className="flex items-start gap-10 cursor-pointer">
+          <span className="text-gray-700 font-medium text-nowrap">
+            Insérez une image :
+          </span>
+          <input
+            type="file"
+            className="block w-full text-sm text-gray-500 
+               file:mr-4 file:py-2 file:px-4
+               file:rounded-lg file:border-0
+               file:text-sm file:font-semibold
+               file:bg-[#06B9AE] file:text-white
+               hover:file:bg-[#059e95]
+               cursor-pointer"
+          />
+        </label>
+      </form>
+    </div>
+  );
+};
+
+export default accessory;
--- app/[nom_boutique]/profile/[category]/addModel/components/shoe.tsx
+++ app/[nom_boutique]/profile/[category]/addModel/components/shoe.tsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React from "react";
 
 const shoe = () => {
+  const sizes = ["39", "40", "41", "42", "43", "44", "45"];
+  const Colors = ["Black", "Brown"];
+
   return (
     <div>
-      addshoe
+      <form className="flex flex-col justify-center items-start mx-auto w-fit space-y-8">
+        <label
+          htmlFor="model"
+          className="flex justify-start items-center gap-10"
+        >
+          <span className="text-xl">Model :</span>
+          <input
+            type="text"
+            placeholder="Model"
+            className="bg-[#B6FFF6] px-5 py-2 rounded-xl border-2 border-[#36CBC1] placeholder:text-gray-600 focus-within:placeholder:text-[#36CBC1] focus-within:outline-0"
+          />
+        </label>
+        <label htmlFor="color" className="flex gap-10 justify-between w-2/5 ">
+          <span className="text-xl text-nowrap">
+            selectionner les couleurs :
+          </span>
+          <ul className="flex gap-10 ">
+            {Colors.map((color) => (
+              <li key={color} className="flex gap-3">
+                <input type="checkbox" />
+                {color}
+              </li>
+            ))}
+          </ul>
+        </label>
+        <label
+          htmlFor="taille"
+          className="flex gap-10 justify-between w-2/5 text-nowrap "
+        >
+          <span className="text-xl">
+            selectionner les pointures disponible :
+          </span>
+          <ul className="flex gap-7">
+            {sizes.map((size) => (
+              <li key={size}>
+                <input type="checkbox" />
+                <p className="text-nowrap">{size}</p>
+              </li>
+            ))}
+          </ul>
+        </label>
+        <label className="flex items-start gap-10 cursor-pointer">
+          <span className="text-gray-700 font-medium text-nowrap">
+            Insérez une image :
+          </span>
+          <input
+            type="file"
+            className="block w-full text-sm text-gray-500 
+               file:mr-4 file:py-2 file:px-4
+               file:rounded-lg file:border-0
+               file:text-sm file:font-semibold
+               file:bg-[#06B9AE] file:text-white
+               hover:file:bg-[#059e95]
+               cursor-pointer"
+          />
+        </label>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default shoe
+export default shoe;
